49b3ef0d7cab695a5936a17d22818a32849ee5e0
leninsvg <lenin@aktiun.com>
2023-03-25 07:35:52 -0500

    Add Base functionality

--- DroneDeliveryService.Models/TripModel.cs
+++ DroneDeliveryService.Models/TripModel.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace DroneDeliveryService.Models;
 
 public class TripModel
--- DroneDeliveryService.Test/DroneDeliveryServiceTest.cs
+++ DroneDeliveryService.Test/DroneDeliveryServiceTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using DroneDeliveryService.Models;
 using DroneDeliveryService.Services;
 
